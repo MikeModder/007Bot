16105800470f25e804ffcde20ad037372ea799fb
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-10-01 12:21:52 -0500

    Inverted this

--- commands/nick.js
+++ commands/nick.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args) => {
-    if(message.guild.me.permissions.has("MANAGE_NICKNAMES")){
+    if(!message.guild.me.permissions.has("MANAGE_NICKNAMES")){
         message.channel.send(":x: I don't have permission to set nicknames!")
         return;
     }
 
     let newNick = args.join(" ")
-    
+
     if(newNick===""){
-        message.channel.send(":white_check_mark: Ok, I've reset your nickname!B")        
+        message.channel.send(":white_check_mark: Ok, I've reset your nickname!B")
         message.member.setNickname("");
         return;
     }

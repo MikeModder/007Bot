5f29c843ed2e039bec6aa5324ba79f66dac43faa
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-10-03 16:09:29 -0500

    Added guild info command

---
+++ commands/guild.js
@@ -0,0 +1,4 @@
+exports.run = (client, message, args, config) => {
+  let codeBlock = "```";
+  message.channel.send(`${codeBlock}Info about ${message.guild.name}:\nOwner: ${message.guild.owner.tag}\nMembers: ${message.guild.members.size}\nChannels: ${message.guild.channels.size} ${codeBlock}`)
+};

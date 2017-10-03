d1689af7595ffc67811430ae4d850214181fd9a1
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-10-03 15:56:02 -0500

    Add perms error output

--- commands/eval.js
+++ commands/eval.js
@@ -1,25 +1,28 @@
 exports.run = (client, message, args, config) => {
-	
-	
-	
+
+
+
 	function clean(text) {
 		if (typeof(text) === "string")
 		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
 	else
 		return text;
-	}	
+	}
 
-	if(message.author.id !== config.owner) return;
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
+	if(message.author.id !== config.owner){
+		message.channel.send(":x: You don't have permission to run that command!")
+		return;
+	}
+  try {
+    const code = args.join(" ");
+    let evaled = eval(code);
 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
+    if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
 
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
+    message.channel.send(clean(evaled), {code:"xl"});
+  } catch (err) {
+    message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+  }
 
 };

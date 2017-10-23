73d6973fd5202e925bb239b943d785dc181d41e6
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-10-23 16:21:06 -0400

    Rounded ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args, config) => {
-  message.channel.send(":ping_pong: -- Pong! The current ping is "+client.ping+"ms.");
+  message.channel.send(":ping_pong: -- Pong! The current ping is "+Math.round(client.ping)+"ms.");
 };

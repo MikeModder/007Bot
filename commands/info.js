6ae15aa392909d6f59b245b1bb4c780841be8188
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-10-23 17:30:44 -0400

    Update info command. Hopefully fixing mentioning bug

--- commands/info.js
+++ commands/info.js
@@ -1,15 +1,16 @@
 exports.run = (client, message, args) => {
     //message.channel.send("007Bot: info placeholder!")
     if(message.mentions.members.size === 0){
-        message.channel.send("Info about `"+message.author.tag+"`:\n" +
-        "Username: "+message.author.username +"\n")
+        let usr = message.author;
+        message.channel.send(`Info about \`'${usr.tag}\`:\n`+
+        `Username: ${usr.username}\n`+
+        `ID: ${usr.id}`)
         return;
     } else {
         let usr = message.mentions.members.first();
-        let usrTag = usr.tag;
-        let usrName = usr.username;
-        message.channel.send('Info about `'+usrTag+"`:\n"+
-        "Username: "+usrName+"\n")
+        message.channel.send(`Info about \`'${usr.tag}\`:\n`+
+        `Username: ${usr.username}\n`+
+        `ID: ${usr.id}`)
         return;
     }
 };

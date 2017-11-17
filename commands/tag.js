b5c88c03186b74c084c764786d129e98b5402d17
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-11-16 20:55:37 -0500

    Make sure users can't mention here or everyone

--- commands/tag.js
+++ commands/tag.js
@@ -14,6 +14,11 @@ exports.run = (client, message, args) => {
         break;
       }
 
+      if(message.mentions.everyone){
+        message.channel.send(':x: Don\'t mention `@here` or `@everyone` in your tag!');
+        break;
+      }
+
       args.shift();
       args.shift();
 

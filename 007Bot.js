a2ce15a1271b09e603acb78a589ace5b8392291c
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-11-16 19:51:59 -0500

    Man I'm bad at this whole renaming variables thing

--- 007Bot.js
+++ 007Bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
   //console.log("007Bot ready! Serving "+client.users.size+" users in "+client.guilds.size+" servers!");
   console.log(`Bot is ready! Logged in as ${client.user.username}.\nServing ${client.users.size} servers, in ${client.guilds.size} servers.`)
   //client.user.setGame(`Say ${config.prefix}help for help! | `+client.guilds.size+' servers | '+client.users.size+' users.');
-  client.user.setGame(`Say ${config.prefix}help for help! | In ${client.guilds.size} servers | ${client.users.size} users.`)
+  client.user.setGame(`Say ${client.config.prefix}help for help! | In ${client.guilds.size} servers | ${client.users.size} users.`)
 });
 
 client.config = require('./config.json');

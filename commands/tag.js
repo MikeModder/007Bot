d067c8bae7673b4e8e45d917c13f5e64f1db7aea
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-11-16 19:44:19 -0500

    It might work

--- commands/tag.js
+++ commands/tag.js
@@ -1,4 +1,8 @@
 exports.run = (client, message, args) => {
+
+  let subCmd = args[0];
+  let tagName = args[1];
+
   switch (args[0]) {
     case "create":
       //create a tags
@@ -9,14 +13,24 @@ exports.run = (client, message, args) => {
       let tagData = [];
       let moment = require('moment');
       args.shift();
-      tagData.author = message.author.tag;
+      args.shift();
+      tagData.author = [];
+      tagData.author.tag = message.author.tag;
+      tagData.author.id = message.author.id;
       tagData.content = args.join(" "); //erase the first arg and join the rest
       tagData.createdAt = []
       tagData.createdAt.hr = moment().format('MMMM Do YYYY, h:mm:ss a'); //current date and time
       tagData.createdAt.cpu = moment().format('YYYYMMDD'); //used for (time ago) in info subcommand
+      let tagJSON = JSON.stringify(tagData);
+      client.tags.set(tagName, tagJSON);
+      message.channel.send(`:white_check_mark: Tag \`${args[0]}\` created!`)
+      break;
     case "edit":
       //edit a tag
       message.channel.send('Coming soon:tm:');
+      break;
+    case "delete":
+      message.channel.send('Coming soon:tm');
       break;
     case "info":
       //Get info about a tag

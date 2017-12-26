7bf655d4bf8542534d79f987f6ca586d68e37d20
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-12-26 14:43:28 -0500

    Add QR generation

---
+++ commands/qr.js
@@ -0,0 +1,20 @@
+exports.run = (client, message, args) => {
+
+    let qrImg = require('qr-image');
+    let validator = require('validator');
+    let Attachment = require('discord.js').Attachment;
+
+    let encodeText = args.join(' ');
+    
+    if(validator.isEmpty(encodeText)){
+        message.channel.send(`:x: You must provide text to encode!`);
+        return;
+    }
+
+    let qr = qrImg.imageSync(encodeText, { type: 'png' });
+    let msgAttach = new Attachment(qr, "qr.png");
+
+    message.channel.send('Here\'s your QR!', msgAttach);
+
+};
+  

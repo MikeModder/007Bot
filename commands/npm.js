1979e0f683a600b6c20593fb6ddd5ec89e7bde8e
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-12-26 15:19:08 -0500

    Add npm command

---
+++ commands/npm.js
@@ -0,0 +1,32 @@
+exports.run = (client, message, args) => {
+
+    let pkgInfo = require('package-info');
+    let validator = require('validator');
+
+    let pkgName = args.join(' ');
+
+    if(validator.isEmpty(pkgName)){
+        message.channel.send(':x: You must provide a package name to search for!');
+        return;
+    }
+
+    pkgInfo(pkgName)
+        .then((info) => {
+            let msg = ``;
+            msg += `Info for package \`${info.name}\`:\n`;
+            msg += `Author: ${info.author}\n`;
+            msg += `Version: ${info.version}\n`;
+            msg += `Description: \`${info.description}\`\n`;
+            msg += `Homepage: ${info.homepage}`;
+    
+            message.channel.send(msg);
+        })
+        .catch((err) => {
+            if(err.statusCode === 404){
+                message.channel.send(`:x: There is no package named \`${pkgName}\`!`);
+            } else {
+                console.log(`[CMD][NPM] Error: ${err}`);
+                message.channel.send(`:x: There was an error trying to get information for the requested package. If you think this is an error, report it to \`mikemodder007#7678\`!`);
+            }
+        });
+};

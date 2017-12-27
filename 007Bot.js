28c540acc57eb0ed8e233943229f4f98b7cf36a7
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-12-26 21:09:37 -0500

    Last commit for tonight: Add info when joining/leaving guilds

--- 007Bot.js
+++ 007Bot.js
@@ -46,12 +46,12 @@ client.on("message", message => {
 });
 
 client.on('guildCreate', (guild) => {
-  client.addLog(`Joined guild \`${guild.name}\` owned by \`${guild.owner.user.username}\` with \`${guild.memberCount}\` members.`);
+  client.addLog(`Joined guild \`${guild.name}\` owned by \`${guild.owner.user.username}\` with \`${guild.memberCount}\` members.\nNow in \`${client.guilds.size}\` servers with \`${client.users.size}\` users.`);
   client.user.setGame(`Say ${client.config.prefix}help for help! | In ${client.guilds.size} servers | ${client.users.size} users.`);
 });
 
 client.on('guildDelete', (guild) => {
-  client.addLog(`Left guild \`${guild.name.replace('@', '')}\` owned by \`${guild.owner.user.tag}\` with \`${guild.memberCount}\` members.`);
+  client.addLog(`Left guild \`${guild.name.replace('@', '')}\` owned by \`${guild.owner.user.tag}\` with \`${guild.memberCount}\` members.\nNow in \`${client.guilds.size}\` servers with \`${client.users.size}\` users.`);
   client.user.setGame(`Say ${client.config.prefix}help for help! | In ${client.guilds.size} servers | ${client.users.size} users.`);
 });
 

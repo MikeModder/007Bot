acfa4c0cbaeb1c63a777491d055e5ab50319a36c
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-12-26 20:19:16 -0500

    Add logging, and update example config

--- 007Bot.js
+++ 007Bot.js
@@ -2,15 +2,22 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const enmap = require("enmap");
 const enmapLevel = require("enmap-level");
-
-client.on("ready", () => {
-  console.log(`Bot is ready! Logged in as ${client.user.username}.\nServing ${client.users.size} servers, in ${client.guilds.size} servers.`);
-  client.user.setGame(`Say ${client.config.prefix}help for help! | In ${client.guilds.size} servers | ${client.users.size} users.`);
-});
+const moment = require('moment');
 
 client.config = require('./config.json');
 client.tags = new enmap({provider: new enmapLevel({name: "tags"})});
 client.ignores = new enmap({provider: new enmapLevel({name: "ignores"})});
+
+client.on("ready", () => {
+  console.log(`Bot is ready! Logged in as ${client.user.username}.\nServing ${client.users.size} servers, in ${client.guilds.size} servers.`);
+  if(client.config.logs.enabled){
+    client.addLog = (text) => {
+      client.guilds.get(client.config.logs.channelId).send(`[${moment().format('lll')}] ${text}`);
+    };
+  }
+  client.addLog(`Bot started!`);
+  client.user.setGame(`Say ${client.config.prefix}help for help! | In ${client.guilds.size} servers | ${client.users.size} users.`);
+});
 
 client.tags.defer.then(() => {
   console.log(`[TAGS] ${client.tags.size} tags loaded!`);

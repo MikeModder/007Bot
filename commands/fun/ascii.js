c3c1745a66efcb472505dfe341114da26729abfd
MikeModder <15313620+MikeModder@users.noreply.github.com>
2017-12-31 19:29:55 -0500

    Ad

---
+++ commands/fun/ascii.js
@@ -0,0 +1,35 @@
+exports.run = (client, message, args) => {
+    
+    let which;
+    switch(args[0]){
+        case "cat":
+            which = true;
+            break;
+        case "cool":
+            which = false;
+            break;
+        default:
+            which = Math.round(Math.random());
+            break;
+    }
+
+    let cat = require('cat-ascii-faces');
+    let cool = require('cool-ascii-faces');
+
+    if(which){
+        //cat
+        message.channel.send(cat());
+    } else {
+        //cool
+        message.channel.send(cool());
+    }
+    
+};
+
+exports.cfg = {
+    name: 'ascii',
+    desc: 'Get a random ASCII face.',
+    usage: '{prefix}ascii',
+    aliases: [ 'face', 'donger' ],
+    public: true
+}

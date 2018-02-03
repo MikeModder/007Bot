00829a81455f8ee90c8f462b805c70ac4b52488b
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-02-03 18:57:54 -0500

    Add thumbnail, and fix my discrim

--- commands/bot/about.js
+++ commands/bot/about.js
@@ -6,12 +6,13 @@ exports.run = (client, message, args) => {
     
     shell.exec('git rev-parse --short HEAD', { silent: true }, (code, out) => {
         embed.setTitle(`About ${client.user.tag}`)
+            .setThumbnail(client.user.avatarURL)
             .addField('Commit ', out, true)
             .addField('NodeJS Version', process.versions.node, true)
             .addField('Instance hosted by', client.users.get(client.config.owner).tag, true)
             .addField('Commands loaded', client.commands.size, true)
             .addField('Commands ran', client.commandsRan, true)
-            .addField('Programmed by', 'mikemodder007#7676 (152541437068705793)');
+            .addField('Programmed by', 'mikemodder007#7678 (152541437068705793)');
 
         message.channel.send(embed);
     })

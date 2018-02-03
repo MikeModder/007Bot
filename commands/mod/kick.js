06f0d74071fe753b436863ba5c5c42071a9c5ae1
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-02-03 14:15:59 -0500

    Quick fix for being able to ban/kick yourself

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -1,35 +1,43 @@
 exports.run = (client, message, [mention, ...reason]) => {
+
+    kMember = message.mentions.members.first();
+
     if(!message.member.permissions.has('KICK_MEMBERS') || !message.author.id === client.config.owner){
-        message.channel.send(":x: You don't have permission to run that command!");
+        message.channel.send(':x: You don\'t have permission to run that command!');
         return;
     }
 
     if(message.mentions.members.size === 0){
-        message.channel.send(":x: Error! You must mention a user to kick!");
+        message.channel.send(':x: Error! You must mention a user to kick!');
         return;
     }
 
-    if(!message.guild.me.permissions.has("KICK_MEMBERS")){
-        message.channel.send(":x: Error! I don't have permission to kick users!");
+    if(!message.guild.me.permissions.has('KICK_MEMBERS')){
+        message.channel.send(':x: Error! I don\'t have permission to kick users!');
         return;
     }
 
-    kMember = message.mentions.members.first();
+    if(kMember.id === message.author.id){
+        message.channel.send(':x: Error! You can\'t kick yourself!')
+    }
 
-    let fullReason = reason.join(" ") || "No reason given.";
+    let fullReason = reason.join(' ') || 'No reason given.';
 
     kMember.send(`You have been kicked from the server **${message.guild.name}**\nReason: `+fullReason);
 
-    kMember.kick(fullReason).then(member => {
+    kMember.ban(fullReason).then(member => {
         message.channel.send(`User **${member.user.username}** was kicked!`);
         return;
+    })
+    .catch(e => {
+        message.channel.send(':x: There was an error kicked the user!')
     });
 };
 
 exports.cfg = {
-    name: 'kick',
-    desc: 'Kicks the (first) mentioned user.',
-    usage: '{prefix}kick [@User]',
+    name: 'ban',
+    desc: 'Bans the (first) mentioned user.',
+    usage: '{prefix}ban [@User]',
     aliases: [],
     public: true
 };

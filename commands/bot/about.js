b6a93064247e0f60f34ccb19611c7375471f7684
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-02-03 14:50:46 -0500

    Add about command with bot info

---
+++ commands/bot/about.js
@@ -0,0 +1,24 @@
+exports.run = (client, message, args) => {
+
+    const Discord = require('discord.js');
+    const shell = require('shelljs');
+    let embed = new Discord.RichEmbed();
+    
+    shell.exec('git rev-parse --short HEAD', { silent: true }, (code, out) => {
+        embed.setTitle(`About ${client.user.tag}`)
+            .addField('Commit ', out, true)
+            .addField('Instance hosted by', client.users.get(client.config.owner).tag)
+            .addField('Programmed by', 'mikemodder007#7676 (152541437068705793)');
+
+        message.channel.send(embed);
+    })
+
+}
+
+exports.cfg = {
+    name: 'about',
+    desc: 'Gives some information on the bot.',
+    usage: '{prefix}about',
+    aliases: [],
+    public: true
+};

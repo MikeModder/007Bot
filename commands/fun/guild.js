fbe579c4c117771e3e3ad66446cb353f964c5897
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-02-05 19:53:30 -0500

    Update guild command to show user count and bot count on different fields

--- commands/fun/guild.js
+++ commands/fun/guild.js
@@ -3,10 +3,12 @@ exports.run = (client, message, args) => {
   let embed = new Discord.RichEmbed();
 
   let guild = message.guild;
+  let botCount = message.guild.members.filter(u => u.user.bot).size;
 
   embed.setTitle(`Info about ${guild.name}`)
     .addField('Owner: ', guild.owner.user.tag, true)
-    .addField('Members: ', guild.memberCount, true)
+    .addField('Users: ', guild.memberCount-botCount, true)
+    .addField('Bots: ', botCount, true)
     .addField('Channels: ', guild.channels.size, true)
     .addField('Region: ', guild.region, true);
 
